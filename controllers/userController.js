9789efdca6b2dac0ac28b7d1edc119af9d43c035
Qazi Ahmad <qaziahmad1877@gmail.com>
2023-05-25 23:47:24 +0500

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,14 +2,12 @@ const userModel = require('../models/userModel');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-const loginController = async (req, res) => {
+const registerController = async (req, res) => {
   try {
     const { name, email, password } = req.body;
-    const userFound = await userModel
-      .findOne({
-        email: email,
-      })
-      .exec();
+    const userFound = await userModel.findOne({
+      email: email,
+    });
     if (userFound) {
       return res
         .status(200)
@@ -18,7 +16,7 @@ const loginController = async (req, res) => {
     // const hashedPassword = await bcrypt.hash(password, 10);
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
-    const newUser = new userModel({
+    const newUser = await new userModel({
       name: name,
       email: email,
       password: hashedPassword,
@@ -33,7 +31,38 @@ const loginController = async (req, res) => {
     });
   }
 };
-const registerController = () => {};
+const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await userModel.findOne({ email: email });
+    if (!user) {
+      return res
+        .status(200)
+        .send({ message: 'user not found', success: false });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res
+        .status(200)
+        .send({ message: 'Invalid Email or Password', success: false });
+    }
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: '1d' }
+    );
+    res
+      .status(200)
+      .send({ message: 'Login Successfully', success: true, token });
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .send({ message: `Error in login Controller ${error.message}` });
+  }
+};
 
 module.exports = {
   loginController,
