53217b19a9b816510f1eb1790099531653f22abb
Qazi Ahmad <qaziahmad1877@gmail.com>
2023-05-26 21:11:30 +0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,44 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Spinner from './components/Spinner/Spinner';
+import { useSelector } from 'react-redux';
+import ProtectedRoutes from './components/ProtectedRoutes/ProtectedRoutes';
+import PublicRoutes from './components/PublicRoutes/PublicRoutes';
 function App() {
+  const { loading } = useSelector((state) => state.alerts);
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoutes>
+                <Home />
+              </ProtectedRoutes>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <PublicRoutes>
+                <Login />
+              </PublicRoutes>
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <PublicRoutes>
+                <Register />
+              </PublicRoutes>
+            }
+          />
+        </Routes>
+      )}
     </>
   );
 }
