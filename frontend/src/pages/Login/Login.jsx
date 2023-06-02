6ad3db36567a9e227c64327145e594f7038f1c60
Qazi Ahmad <qaziahmad1877@gmail.com>
2023-06-02 23:40:57 +0500

    login

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,23 +1,30 @@
 import { Form, Input, message } from 'antd';
-import '../styles/Login.scss';
+import './Login.scss';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { hideLoading, showLoading } from '../../redux/features/alertSlice';
 
 const Login = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   // form handler
   const host = 'http://localhost:4000';
   const onfinishHandler = async (values) => {
     try {
+      dispatch(showLoading());
       const res = await axios.post(`${host}/api/user/login`, values);
+      window.location.reload();
+      dispatch(hideLoading());
       if (res.data.success) {
+        localStorage.setItem('token', res.data.token);
         message.success('Login Success');
         navigate('/');
-      }
-      else{
+      } else {
         message.error(res.data.message);
       }
     } catch (error) {
+      dispatch(hideLoading());
       console.log(error);
       message.error('Something went wrong');
     }
