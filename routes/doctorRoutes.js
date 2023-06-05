7448964a0b11b4dcd2791efa901c4ce2a3e7cbb4
Qazi Ahmad <qaziahmad1877@gmail.com>
2023-06-05 21:38:07 +0500

    Update doctorRoutes.js

--- routes/doctorRoutes.js
+++ routes/doctorRoutes.js
@@ -3,9 +3,11 @@ const authMiddleware = require('../middlewares/authMiddleware');
 const {
   getDoctorInfoController,
   updateProfileController,
+  getDoctorById,
 } = require('../controllers/doctorController');
 const router = express.Router();
 
 router.post('/getDoctorInfo', authMiddleware, getDoctorInfoController);
 router.post('/updateProfile', authMiddleware, updateProfileController);
+router.post('/getDoctorById', authMiddleware, getDoctorById);
 module.exports = router;
