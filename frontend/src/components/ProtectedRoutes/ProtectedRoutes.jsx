e5851c62fcc29f6fa0afbb6a849505d46868da1a
Qazi Ahmad <qaziahmad1877@gmail.com>
2023-06-13 12:20:53 +0500

    frontend

--- frontend/src/components/ProtectedRoutes/ProtectedRoutes.jsx
+++ frontend/src/components/ProtectedRoutes/ProtectedRoutes.jsx
@@ -7,29 +7,28 @@ import { setUser } from '../../redux/features/userSlice';
 
 export default function ProtectedRoutes({ children }) {
   const dispatch = useDispatch();
-  const navitate = useNavigate();
   const host = 'http://localhost:4000';
   const { user } = useSelector((state) => state.user);
+  // eslint-disable-next-line
   const getUser = async () => {
+    const headers = { Authorization: localStorage.getItem('token') };
     try {
       dispatch(showLoading());
       const res = await axios.post(
         `${host}/api/user/getUserData`,
-        { token: localStorage.getItem('token') },
-        {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem('token')}`,
-          },
-        }
+        {},
+        { headers }
       );
       dispatch(hideLoading());
       if (res.data.success) {
         dispatch(setUser(res.data.data));
       } else {
         <Navigate to="/login" />;
+        localStorage.clear();
       }
     } catch (error) {
       dispatch(hideLoading());
+      localStorage.clear();
       console.log(error);
     }
   };
@@ -37,7 +36,8 @@ export default function ProtectedRoutes({ children }) {
     if (!user) {
       getUser();
     }
-  }, []);
+  }, [user, getUser]);
+
   if (localStorage.getItem('token')) {
     return children;
   } else {
