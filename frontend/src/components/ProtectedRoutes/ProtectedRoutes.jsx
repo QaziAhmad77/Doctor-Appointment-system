a019ad531a5ab4fd95f259e673c36f31c8a90199
Qazi Ahmad <qaziahmad1877@gmail.com>
2023-08-28 21:43:45 +0500

    Update ProtectedRoutes.jsx

--- frontend/src/components/ProtectedRoutes/ProtectedRoutes.jsx
+++ frontend/src/components/ProtectedRoutes/ProtectedRoutes.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Navigate, useNavigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { hideLoading, showLoading } from '../../redux/features/alertSlice';
 import axios from 'axios';
 import { setUser } from '../../redux/features/userSlice';
